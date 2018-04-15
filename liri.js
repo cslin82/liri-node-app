1647952968c6e975e5352d4d192e982bcee54571
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-04-14 23:45:18 -0400

    Add logic to retrieve arguments

--- liri.js
+++ liri.js
@@ -1,1 +1,10 @@
 require("dotenv").config();
+
+// Get command-line arguments
+
+var [ , , ...myArgs] = process.argv;
+var argAction = myArgs.shift();
+myArgs = myArgs.join(' ');
+
+console.log(`action is ${argAction} and target is ${myArgs}`);
+
